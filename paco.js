dec9fe323f65a2c2a96bc36dadb921a921502621
Rui Azevedo <ruihfazevedo@gmail.com>
2020-11-25 18:35:26 -0100

    parser boot for continuations at root level

--- paco.js
+++ paco.js
@@ -31,6 +31,10 @@ patchPrimitives(
   Array().__proto__,
 )
 
+//////////////////////////////////////////////////////////
+// Parser
+
+//parser primitves
 const inRange=(a,z)=>o=>a<=o&&o<=z
 const isDigit=inRange('0','9')
 isDigit.expect="digit"
@@ -39,27 +43,28 @@ const isUpperCase=inRange('A','Z')
 const isLetter=o=>isLowerCase(o)||isUpperCase(o)
 const isAlphaNum=o=>isLetter(o)||isDigit(o)
 
-//recursively extends the parser continuations (.then, .drop, .or)
+//recursively extends the parser continuations (.then, .drop, .or, ...)
 const parserOf=o=>(
   o.then=p=>parserOf(io=>io.mbind(o).mbind(p))//o.then(p) <=> \io-> io >>= o >>= p
   ,o.drop=p=>parserOf(io=>{
     const os=io.mbind(o)
-    return os.mbind(p).map(map(o=>[snd(fromRight(os))])).when(os)
+    return os.mbind(p).map(map(o=>snd(fromRight(os)))).when(os)
   })
   ,o.or=p=>parserOf(io=>o(io).or(p(io)))
   ,o)
 
-const satisfy=chk=>parserOf(io=>{
-  const r=chk(head(io.fst()))
-  return r?
+//and id parse combinator to apply continuations on root elements
+const boot=()=>parserOf(fcomp(Right)(id))
+
+const drop=o=>boot().drop(o)//apply drop (continuation) to the root element, using `boot` combinator
+ 
+const satisfy=chk=>parserOf(io=>
+  chk(head(io.fst()))?
     Right(//success...
       Pair(//build a pair of remaining input and composed output
         tail(io.fst()),//consume input
         io.snd().append([head(io.fst())])))//compose the outputs
-    :Left(chk.expect)
-})
-
-const drop=o=>parserOf(io=>io.mbind(o).when(io).map(map(o=>[snd(io)])))
+    :Left(chk.expect))
 
 const digit=satisfy(isDigit)
 const lowerCase=satisfy(isLowerCase)
@@ -74,7 +79,7 @@ console.log(
     .mbind(digit)
     .mbind(digit)
     .mbind(digit)
-    // .map(map(o=>[o.join("")]))
+    .map(map(o=>[o.join("")]))
 )
 
 console.log(
@@ -82,7 +87,7 @@ console.log(
     .drop(digit)
     .then(digit)
     (Pair("123",[]))
-    // .map(map(o=>[o.join("")]))
+    .map(map(o=>[o.join("")]))
 )
 
 console.log(
@@ -90,9 +95,9 @@ console.log(
     .then(digit)
     .then(digit)
     (Pair("123",[]))
-    // .map(map(o=>[o.join("")]))
+    .map(map(o=>[o.join("")]))
 )
 
-clog(letter.or(digit)(Pair("a12",[])))
+clog(letter.or(digit)(Pair("0a12",[])))
 
 var io=Pair("123",[])
